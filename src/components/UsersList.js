263360c691d4901b72f797cd40ff7efbb97096ac
sabrinamaral <sabrinamaral@hotmail.com>
2022-12-17 10:57:27 -0300

    fetching data with AsyncThunk

--- src/components/UsersList.js
+++ src/components/UsersList.js
@@ -1,4 +1,14 @@
+import { useEffect } from "react";
+import { useDispatch } from "react-redux";
+import { fetchUsers } from "../store";
+
 const UsersList = () => {
+  const dispatch = useDispatch();
+  useEffect(() => {
+    dispatch(fetchUsers());
+    // eslint-disable-next-line
+  }, []);
+
   return <div>UsersList</div>;
 };
 
