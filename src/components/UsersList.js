e9d6f53fb25d48d4d36fe195d6075b34cb7dd906
sabrinamaral <sabrinamaral@hotmail.com>
2022-12-18 11:19:27 -0300

    use the custom hook

--- src/components/UsersList.js
+++ src/components/UsersList.js
@@ -1,15 +1,59 @@
 import { useEffect } from "react";
-import { useDispatch } from "react-redux";
-import { fetchUsers } from "../store";
+import { useSelector } from "react-redux";
+import { fetchUsers, addUser } from "../store";
+import useThunk from "../hooks/use-thunk";
+import Button from "./Button";
+import Skeleton from "./skeleton";
 
 const UsersList = () => {
-  const dispatch = useDispatch();
-  useEffect(() => {
-    dispatch(fetchUsers());
-    // eslint-disable-next-line
-  }, []);
+  const [doFetchUsers, isLoadingUsers, loadingUsersError] =
+    useThunk(fetchUsers);
+  const [doCreateUser, isCreatingUser, creatingUserError] = useThunk(addUser);
 
-  return <div>UsersList</div>;
+  const { data } = useSelector((state) => {
+    return state.users;
+  });
+  useEffect(() => {
+    doFetchUsers();
+  }, [doFetchUsers]);
+
+  const handleUserAdd = () => {
+    doCreateUser();
+  };
+  let content;
+
+  if (isLoadingUsers) {
+    content = <Skeleton times={6} className="h-10 w-full " />;
+  } else if (loadingUsersError) {
+    content = <div>There is an error loading data...</div>;
+  } else {
+    content = data.map((user) => {
+      return (
+        <div key={user.id} className="mb-2 border rounded">
+          <div className="flex p-2 justify-between items-center cursor-pointer">
+            {user.name}
+          </div>
+        </div>
+      );
+    });
+  }
+  return (
+    <div>
+      <div className="flex flex-row justify-between items-center m-3">
+        <h1 className="m-2 text-xl">Users</h1>
+        <Button
+          loading={isCreatingUser}
+          primary
+          rounded
+          onClick={handleUserAdd}
+        >
+          + add user
+        </Button>
+        {creatingUserError && "Error creating user..."}
+      </div>
+      {content}
+    </div>
+  );
 };
 
 export default UsersList;
