8f188ac27e13bc1493d7bc5ce68f07f2c10e1a5e
sabrinamaral <sabrinamaral@hotmail.com>
2022-12-18 11:20:43 -0300

    addUser thunk

--- src/store/slices/usersSlice.js
+++ src/store/slices/usersSlice.js
@@ -1,4 +1,5 @@
 import { createSlice } from "@reduxjs/toolkit";
+import { addUser } from "../thunks/addUser";
 import { fetchUsers } from "../thunks/fetchUsers";
 
 const usersSlice = createSlice({
@@ -17,6 +18,17 @@ const usersSlice = createSlice({
       state.data = action.payload;
     });
     builder.addCase(fetchUsers.rejected, (state, action) => {
+      state.isLoading = false;
+      state.error = action.error;
+    });
+    builder.addCase(addUser.pending, (state, action) => {
+      state.isLoading = true;
+    });
+    builder.addCase(addUser.fulfilled, (state, action) => {
+      state.isLoading = false;
+      state.data.push(action.payload);
+    });
+    builder.addCase(addUser.rejected, (state, action) => {
       state.isLoading = false;
       state.error = action.error;
     });
