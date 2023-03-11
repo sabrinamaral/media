be0aa7456b6f66b161a53529d1eacfc5bf2c557b
sabrinamaral <sabrinamaral@hotmail.com>
2023-03-11 17:38:43 -0300

    small adjustment
    changing isLoading for isFetching

--- src/components/AlbumsList.js
+++ src/components/AlbumsList.js
@@ -9,13 +9,13 @@ import Button from "./Button";
 import AlbumsListItem from "./AlbumsListItem";
 
 const AlbumsList = ({ user }) => {
-  const { data, error, isLoading } = useFetchAlbumsQuery(user);
+  const { data, error, isFetching } = useFetchAlbumsQuery(user);
   const [addAlbum, results] = useAddAlbumMutation();
   const handleAddAlbum = () => {
     addAlbum(user);
   };
   let content;
-  if (isLoading) {
+  if (isFetching) {
     content = <Skeleton className={"h-10 w-full"} times={3} />;
   } else if (error) {
     <div>Error loading albums...</div>;
