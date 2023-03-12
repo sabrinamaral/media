f540d351cb9d2305a176165989ac31b4d37ed600
sabrinamaral <sabrinamaral@hotmail.com>
2023-03-11 23:22:06 -0300

    added photosList to AlbumsListItem

--- src/components/AlbumsListItem.js
+++ src/components/AlbumsListItem.js
@@ -2,6 +2,7 @@ import Button from "./Button";
 import ExpandablePanel from "./ExpandablePanel";
 import { GoTrashcan } from "react-icons/go";
 import { useRemoveAlbumMutation } from "../store";
+import PhotosList from "./PhotosList";
 
 function AlbumsListItem({ album }) {
   const [removeAlbum, results] = useRemoveAlbumMutation();
@@ -23,7 +24,7 @@ function AlbumsListItem({ album }) {
   );
   return (
     <ExpandablePanel key={album.id} header={header}>
-      List of photos in the album.
+      <PhotosList album={album} />
     </ExpandablePanel>
   );
 }
