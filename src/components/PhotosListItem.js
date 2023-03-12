6aec7bc0d1c5552776c84d421393a70e3b52f493
sabrinamaral <sabrinamaral@hotmail.com>
2023-03-12 10:56:48 -0300

    finishing PhotosListItem component
    added overlay with a delete icon and the feature removePhoto

--- src/components/PhotosListItem.js
+++ src/components/PhotosListItem.js
@@ -1,7 +1,17 @@
+import { GoTrashcan } from "react-icons/go";
+import { useRemovePhotoMutation } from "../store";
+
 function PhotosListItem({ photo }) {
+  const [removePhoto] = useRemovePhotoMutation();
+  const handleRemovePhoto = () => {
+    removePhoto(photo);
+  };
   return (
-    <div>
+    <div className="relative cursor-pointer m-2">
       <img className="h-20 w-20" src={photo.url} alt="randon pic"></img>
+      <div className="absolute inset-0 flex items-center justify-center hover:bg-gray-200 opacity-0 hover:opacity-80">
+        <GoTrashcan className="text-3xl" onClick={handleRemovePhoto} />
+      </div>
     </div>
   );
 }
